cb1b7e9e4d12a0dd8f5104b244db568df50a147b
Balint Csamango <c2csabal@vasvari.org>
2026-02-18 12:22:09 +0100

    working code

--- Program.cs
+++ Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace akva_projekt
+namespace akva_project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("elolenyek.txt");
+            Dictionary<string, List<string>> elolenyek = new Dictionary<string, List<string>>();
+            while (!sr.EndOfStream)
+            {
+                string[] adatok = sr.ReadLine().Split(';');
+                elolenyek[adatok[0]] = new List<string> { adatok[1], adatok[2], adatok[3], adatok[4], adatok[5]};
+            }
+            foreach (var kvp in elolenyek)
+            {
+                Console.Write($"{kvp.Key} adatai: ");
+                foreach (var adat in kvp.Value)
+                {
+                    Console.Write($"{adat} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
